61d047ac86942ac1cdbf26bfad069d7bcf0f8fd3
Saul Artiga <slbrtg@gail.com>
2017-06-29 08:37:05 -0700

    Add initial files and folders, begin writing first test

---
+++ src/test/java/HangmanTest.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class HangmanTest {
+
+  @Test
+  public void generateRandomWord_RevceiveRandomWord_True(){
+    
+  }
+
+}
